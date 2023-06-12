9aa42fd715630e0c69ce4915800fa13c9495fcfe
Domi02 <46600596+Domi02@users.noreply.github.com>
2023-06-12 14:16:52 +0200

    Login Activity con connessione ai fragment

---
+++ app/src/main/java/com/Kotlette/ecommerce/LoginActivity.kt
@@ -0,0 +1,51 @@
+package com.Kotlette.ecommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import com.Kotlette.ecommerce.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private fun LoginButton(){
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            val myFragment = LoginFragment()
+            fragmentTransaction.add(R.id.fragmentContainerView, LoginFragment())
+            fragmentTransaction.addToBackStack("fragment login")
+            fragmentTransaction.commit()
+            binding.Cl1?.visibility = View.INVISIBLE
+
+        }
+        binding.buttonRegister.setOnClickListener {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            val myFragment = RegisterFragment()
+            fragmentTransaction.add(R.id.fragmentContainerView, RegisterFragment())
+            fragmentTransaction.addToBackStack("fragment register")
+            fragmentTransaction.commit()
+            binding.Cl1?.visibility = View.INVISIBLE
+
+        }
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        LoginButton()
+
+    }
+    override fun onBackPressed() {
+        val fragmentManager = supportFragmentManager
+        fragmentManager.popBackStack()
+        LoginButton()
+    }
+}
