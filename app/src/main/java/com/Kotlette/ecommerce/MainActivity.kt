f6d6dc77c69b2132c512ae5ce05f299012b0db8f
Ramingo <cassanodavide@ymail.com>
2023-06-13 14:20:54 +0200

    Impostazione layout MainActivity, Inserimento NavBar, setappare i fragment

--- app/src/main/java/com/Kotlette/ecommerce/MainActivity.kt
+++ app/src/main/java/com/Kotlette/ecommerce/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.Kotlette.ecommerce
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.Kotlette.ecommerce.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.account -> replaceFragment(ProfileFragment())
+            R.id.home -> replaceFragment(HomeFragment())
+            R.id.cart -> replaceFragment(CartFragment())
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.addToBackStack("fragment register")
+        fragmentTransaction.commit()
     }
 }
