5da001d0475233adc9716064d2ba9e2591d1542e
Ramingo <cassanodavide@ymail.com>
2023-06-14 21:05:58 +0200

    Aggiornamento HomeFragment.kt. Bisogna individuare un metodo per impostare tutte e 3 le recycleView

--- app/src/main/java/com/Kotlette/ecommerce/HomeFragment.kt
+++ app/src/main/java/com/Kotlette/ecommerce/HomeFragment.kt
@@ -5,28 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.Kotlette.ecommerce.adapter.AdapterHome
+import com.Kotlette.ecommerce.item.ItemHome
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var adapter : AdapterHome
+    private lateinit var recyclerView : RecyclerView
+    private lateinit var homeArrayList : ArrayList<ItemHome>
+
+    lateinit var title: Array<String>
+    lateinit var image: Array<String>
+    lateinit var price: Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -37,23 +33,84 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataInitialize()
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        adapter = AdapterHome(homeArrayList)
+        recyclerView.adapter = adapter
     }
+
+    private fun dataInitialize() {
+
+        homeArrayList = arrayListOf<ItemHome>()
+
+        title = arrayOf(
+            "Via Badia 9",
+            "Via Tiepolo 15",
+            "Piazza Navona 45A",
+            "Srada Longevo 8",
+            "Viale Regione Siciliana 77",
+            "Via Cordova 6",
+            "Via M. De Cervantes 2",
+            "Piazza A. Arrigo 4",
+            "Via Badia 9",
+            "Via Tiepolo 15",
+            "Piazza Navona 45A",
+            "Srada Longevo 8",
+            "Viale Regione Siciliana 77",
+            "Via Cordova 6",
+            "Via M. De Cervantes 2",
+            "Piazza A. Arrigo 4"
+        )
+
+        image = arrayOf(
+            "05-05-2552",
+            "20-10-2532",
+            "22-05-2552",
+            "29-03-2082",
+            "10-01-2552",
+            "20-11-2852",
+            "31-11-2002",
+            "20-07-2002",
+            "05-05-2552",
+            "20-10-2532",
+            "22-05-2552",
+            "29-03-2082",
+            "10-01-2552",
+            "20-11-2852",
+            "31-11-2002",
+            "20-07-2002"
+        )
+
+        price = arrayOf(
+            "15,58€",
+            "26,18€",
+            "100,00€",
+            "48,58€",
+            "35,58€",
+            "15,53€",
+            "7,22€",
+            "18,40€",
+            "15,58€",
+            "26,18€",
+            "100,00€",
+            "48,58€",
+            "35,58€",
+            "15,53€",
+            "7,22€",
+            "18,40€"
+        )
+
+        for(i in title.indices){
+
+            val home = ItemHome(title[i], image[i], price[i])
+            homeArrayList.add(home)
+        }
+
+    }
+
 }
