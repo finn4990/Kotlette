1f8da14075a159117898b46916f358b2c397bdb9
Domi02 <46600596+Domi02@users.noreply.github.com>
2023-06-20 19:02:19 +0200

    Piccolo fix ProfileFragment.kt

--- app/src/main/java/com/Kotlette/ecommerce/main/ProfileFragment.kt
+++ app/src/main/java/com/Kotlette/ecommerce/main/ProfileFragment.kt
@@ -117,7 +117,7 @@ class ProfileFragment : Fragment(), AdapterView.OnItemSelectedListener {
     override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
         val category = FileManager(requireContext())
         val Choice = parent.getItemAtPosition(pos).toString()
-        category.writeToFile("category.txt", Choice)
+        category.writeToFile("favorite.txt", Choice)
     }
 
     override fun onNothingSelected(parent: AdapterView<*>) {
